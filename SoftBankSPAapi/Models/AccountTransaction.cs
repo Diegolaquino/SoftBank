705ed4c41121a197c71b4323048f334d1b2b3661
Diego Aquino <diegol.aquino@gmail.com>
2019-11-30 15:37:02 -0300

    create classes

---
+++ SoftBankSPAapi/Models/AccountTransaction.cs
@@ -0,0 +1,25 @@
+﻿using SoftBankSPAapi.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftBankSPAapi.Models
+{
+    public class AccountTransaction
+    {
+        public int AccountTransactionId { get; set; }
+
+        public Account FromAccount { get; set; }
+
+        public int? FromAccountId { get; set; }
+
+        public Account ToAccount { get; set; }
+
+        public int ToAccountId { get; set; }
+
+        public string Description { get; set; }
+
+        public TransactionType TransactionType { get; set; }
+    }
+}
---
+++ SoftBankSPAapi/Models/Enum/TransactionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftBankSPAapi.Models.Enum
+{
+    public enum TransactionType
+    {
+        Payment = 1,
+        Transfer = 2,
+        BankWithdrawal = 3,
+        Purchase = 4
+    }
+}

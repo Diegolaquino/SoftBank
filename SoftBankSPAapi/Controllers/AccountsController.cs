d41bc0c5dc42963ad46340b39af0a752b914181a
Diego Aquino <diegol.aquino@gmail.com>
2019-11-30 16:42:46 -0300

    accounts e git

--- SoftBankSPAapi/Controllers/AccountsController.cs
+++ SoftBankSPAapi/Controllers/AccountsController.cs
@@ -18,9 +18,9 @@ namespace SoftBankSPAapi.Controllers
             var diego = new Customer() { CustomerId = 1, CPF = "12960405773", Email = "Diegol.aquino@gmail.com", Name = "Diego Aquino", Phone = "21 9 92034654" };
             IEnumerable<Account> accounts = new List<Account>()
             {
-                new Account(){ AccountId = 1, AccountNumber = 1234567, AgencyNumber = 1111, Balance = 5000.50M, ContractedServices = new List<AccountantService>(), CustomerId = 1, Customer = diego},
-                new Account(){ AccountId = 1, AccountNumber = 7654321, AgencyNumber = 2222, Balance = 10000.50M, ContractedServices = new List<AccountantService>(), CustomerId = 1, Customer = diego},
-                new Account(){ AccountId = 1, AccountNumber = 7777777, AgencyNumber = 3333, Balance = 15000.50M, ContractedServices = new List<AccountantService>(), CustomerId = 1, Customer = diego}
+                new Account(){ AccountId = 1, AccountNumber = 1234567, AgencyNumber = 1111, Balance = 5000.50M, ContractedProducts = new List<AccountProduct>(), CustomerId = 1, Customer = diego},
+                new Account(){ AccountId = 1, AccountNumber = 7654321, AgencyNumber = 2222, Balance = 10000.50M, ContractedProducts = new List<AccountProduct>(), CustomerId = 1, Customer = diego},
+                new Account(){ AccountId = 1, AccountNumber = 7777777, AgencyNumber = 3333, Balance = 15000.50M, ContractedProducts = new List<AccountProduct>(), CustomerId = 1, Customer = diego}
             };
 
             return accounts.ToList();

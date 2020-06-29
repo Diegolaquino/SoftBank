db7021c97ba0b5d020e8b3e85b56be720218493e
Diego Aquino <diegol.aquino@gmail.com>
2020-06-29 18:19:03 -0300

    instalação do chart e inclusão do pieChart para demostração.

--- softbank-client/src/app/home/home.component.ts
+++ softbank-client/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from '../Models/customer';
 import { Router } from '@angular/router';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,8 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
 
   customer : Customer;
+
+  PieChart : Chart;
 
   constructor(private route: Router) { }
 
@@ -23,6 +26,53 @@ export class HomeComponent implements OnInit {
     var userInfo = JSON.parse(dataUserInfo);
 
     this.customer = userInfo.user;
-  }
 
-}
+    this.PieChart = new Chart ('pieChart', {
+      type:'doughnut',
+      data:{ 
+        labels:["Vestuário", "Alimentação", "Transportes"],
+        datasets:[{
+          label: 'Vote Now',
+          data:[139.12,620.25,438.07],
+          backgroundColor:[
+            'rgba(154, 192, 0, 0.9)',
+            'rgba(60, 203, 218, 0.9)',
+            'rgba(255, 186, 0, 0.9)',
+            'rgba(230, 37, 140, 0.9)',
+            'rgba(135, 2, 123, 0.9)',
+            'rgba(79, 3, 79, 0.9)'
+          ], 
+          hoverBackgroundColor: 'black'
+        }] 
+      },
+      options: {
+            scales: {
+                yAxes: [{
+                  display: false,  
+                  ticks: {
+                        beginAtZero: true
+                    },
+                    gridLines:{
+                      // color: 'rgba(0,0,0,0)'
+                      display:false,
+                      drawBorder:false
+                    }
+                }],
+                xAxes:[{
+                  display:false,
+                  gridLines:{
+                    // 
+                    display: false,
+                    drawBorder:false
+                  }
+                }
+
+                ]
+            },
+            tooltips:{
+              caretSize: 10
+            }
+        }
+    }); }
+  
+  }
